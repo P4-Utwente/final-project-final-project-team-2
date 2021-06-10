ee202a141965f0d803444790922703be6eee303f
Ha Le <nhiha001@gmail.com>
2021-06-11 00:06:00 +0200

    Add gcloud auth rule test TDD

---
+++ thefuck-master/tests/rules/test_gcloud_auth.py
@@ -0,0 +1,18 @@
+from thefuck.rules.gcloud_auth import match, get_new_command
+from thefuck.types import Command
+
+def test_match():
+    response1 = """
+    ERROR: (gcloud.pubsub.subscriptions.list) You do not currently have an active account selected.
+"""
+    assert match(Command('gcloud pubsub subscriptions list', response1))
+
+    response2 = """
+    ERROR: (gcloud.ai-platform) Command name argument expected.
+"""
+    assert not match(Command('gcloud ai-platform', response2))
+
+
+def test_get_new_command():
+    assert get_new_command(Command('gcloud pubsub subscriptions list', '')) == 'gcloud auth login'
+    assert get_new_command(Command('gcloud projects describe the-fuck', '')) == 'gcloud auth login'
---
+++ thefuck-master/thefuck/rules/gcloud_auth.py
@@ -0,0 +1,7 @@
+def match(command):
+    return False
+
+def get_new_command(command):
+    return None
+
+
