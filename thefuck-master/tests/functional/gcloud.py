2fc6ec08404c0ec962462ec4d3cfed0c78f6f975
Ha Le <nhiha001@gmail.com>
2021-06-22 16:38:08 +0200

    Remove gcloud function test

--- thefuck-master/tests/functional/gcloud.py
+++
@@ -1,6 +0,0 @@
-def select_gcloud_auth_command_with_arrows(proc, TIMEOUT):
-    proc.sendline(u'gcloud pubsub subscriptions list')
-    assert proc.expect([TIMEOUT, u"ERROR: (gcloud.pubsub.subscriptions.list) You do not currently have an active account selected."])
-
-    proc.sendline(u'fuck')
-    assert proc.expect([TIMEOUT, u'gcloud auth login && gcloud pubsub subscriptions list'])
