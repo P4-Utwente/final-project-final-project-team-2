803b276e5d9541b6a20c2bf76052b6b6d899686a
VvWijhe <victor.v.wijhe@hotmail.com>
2021-06-24 04:17:23 +0200

    refactored ag_literal.py

--- thefuck-master/thefuck/rules/ag_literal.py
+++ thefuck-master/thefuck/rules/ag_literal.py
@@ -3,8 +3,10 @@ from thefuck.utils import for_app
 
 @for_app('ag')
 def match(command):
+    """A dummy docstring."""
     return command.output.endswith('run ag with -Q\n')
 
 
 def get_new_command(command):
+    """A dummy docstring."""
     return command.script.replace('ag', 'ag -Q', 1)
